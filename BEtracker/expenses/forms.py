3ceccd1fd1f07d75ea2fc3bf2b077314d01ea151
abdelillah <abdelillah.ouraou@gmail.com>
2025-08-08 16:14:35 +0100

    adding the expense form

---
+++ BEtracker/expenses/forms.py
@@ -0,0 +1,11 @@
+# expenses/forms.py
+from django import forms
+from .models import Expense
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['amount', 'category', 'date', 'description']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),  # calendar picker
+        }
--- BEtracker/expenses/views.py
+++ BEtracker/expenses/views.py
@@ -3,6 +3,49 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import Expense
+from .forms import ExpenseForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def expense_list(request):
+    expenses = Expense.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses})
+
+@login_required
+def expense_add(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/expense_form.html', {'form': form})
+
+
+@login_required
+def expense_edit(request, pk):
+    expense = get_object_or_404(Expense, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, 'expenses/expense_form.html', {'form': form})
+
+@login_required
+def expense_delete(request, pk):
+    expense = get_object_or_404(Expense, pk=pk, user=request.user)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('expense_list')
+    return render(request, 'expenses/expense_confirm_delete.html', {'expense': expense})
 
 
 def register(request):
@@ -15,11 +58,4 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
-
-
-@login_required
-def dashboard(request):
-    # View code here
-    return render(request, 'dashboard.html')
-
 # Create your views here.
